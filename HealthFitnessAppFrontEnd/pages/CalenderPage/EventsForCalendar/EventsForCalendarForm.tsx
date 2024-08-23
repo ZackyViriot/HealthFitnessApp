54a797f0facfc3fc6080b7c7c02a914fb2c8fb61
Zacky B Viriot <89651654+ZackyViriot@users.noreply.github.com>
2024-08-23 15:18:53 -0500

    added the event field page

--- HealthFitnessAppFrontEnd/pages/CalenderPage/EventsForCalendar/EventsForCalendarForm.tsx
+++ HealthFitnessAppFrontEnd/pages/CalenderPage/EventsForCalendar/EventsForCalendarForm.tsx
@@ -3,7 +3,7 @@ import { View, Text, TextInput, TouchableOpacity, Animated } from 'react-native'
 import { useNavigation, NavigationProp } from '@react-navigation/native';
 import { RootStackParamList } from '../../../types';
 import { DatePicker } from '../../../components/nativewindui/DatePicker';
-
+import { Picker } from '@react-native-picker/picker';
 
 
 
@@ -28,7 +28,27 @@ const EventsForCalendarForm = () => {
     const [userId, setUserId] = useState("");
 
 
+    const openPicker = () => {
+        setIsPickerVisible(true);
+        Animated.timing(pickerHeight, {
+            toValue: 200, // Height of the picker
+            duration: 300,
+            useNativeDriver: false,
+        }).start();
+    };
 
+    const closePicker = () => {
+        Animated.timing(pickerHeight, {
+            toValue: 0,
+            duration: 300,
+            useNativeDriver: false,
+        }).start(() => setIsPickerVisible(false));
+    };
+
+
+    const handleAddEvent = async () => {
+
+    }
 
     return (
         <View className='flex-1 p-6 bg-gray-100 justify-center'>
@@ -45,7 +65,7 @@ const EventsForCalendarForm = () => {
                 {errors.event ? <Text className='text-red-500'>{errors.event}</Text> : null}
             </View>
             <View className='mb-4 items-center rounded-lg'>
-                <Text className='text-lg mb-2'>Select time of Event</Text>
+                <Text className='text-lg mb-2'>Select importance of Event</Text>
                 <DatePicker
                     value={date}
                     mode='datetime'
@@ -54,10 +74,51 @@ const EventsForCalendarForm = () => {
                     }}
                 />
             </View>
+            <View className='mb-4'>
+                <Text className='text-lg mb-2'>Level of importance </Text>
+                <TouchableOpacity
+                    className={`bg-white p-4 rounded-lg shadow-sm ${errors.importance ? 'border-red-500 border-2' : ''}`}
+                    onPress={openPicker}
+                >
+                    <Text className='text-lg'>
+                        {formData.importance ? `${formData.importance} ` : 'Select Time'}
+                    </Text>
+                </TouchableOpacity>
+                {errors.importance ? <Text className='text-red-500'>{errors.importance}</Text> : null}
+            </View>
 
+            <TouchableOpacity
+                className='bg-black p-2 rounded-lg mb-4'
+                onPress={handleAddEvent}
+            >
+                <Text className='text-white text-center text-lg'>Add Event</Text>
+            </TouchableOpacity>
 
-
+            {/* Animated View for Picker */}
+            {isPickerVisible && (
+                <Animated.View style={{ height: pickerHeight, overflow: 'hidden', backgroundColor: 'white', borderRadius: 10, marginTop: 20 }}>
+                    <Picker
+                        selectedValue={formData.importance}
+                        onValueChange={(value) => {
+                            setFormData({ ...formData, importance: value });
+                            closePicker();
+                        }}
+                        style={{ height: '100%', width: '100%' }}
+                    >
+                        <Picker.Item label='1 Low' value='1' />
+                        <Picker.Item label='2' value='2' />
+                        <Picker.Item label='3 Medium' value='3' />
+                        <Picker.Item label='4' value='4' />
+                        <Picker.Item label='5 High' value='5' />
+                     
+                    </Picker>
+                </Animated.View>
+            )}
         </View>
+
+
+
+
     )
 }
 
